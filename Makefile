44ba1f194c9d64787bf32fa8283f721e15c2e46e
thisisaaronland <devnull@localhost>
2019-03-06 15:15:38 -0800

    initial commit

---
+++ Makefile
@@ -0,0 +1,34 @@
+CWD=$(shell pwd)
+GOPATH := $(CWD)
+
+prep:
+	if test -d pkg; then rm -rf pkg; fi
+
+self:   prep rmdeps
+	if test -d src; then rm -rf src; fi
+	mkdir -p src/github.com/sfomuseum/go-sfomuseum-twitter
+	# cp *.go src/github.com/sfomuseum/go-url-unshortener/
+	cp -r vendor/* src/
+
+rmdeps:
+	if test -d src; then rm -rf src; fi 
+
+build:	fmt bin
+
+deps:
+	@GOPATH=$(GOPATH) go get -u "github.com/tidwall/gjson"
+
+vendor-deps: rmdeps deps
+	if test ! -d vendor; then mkdir vendor; fi
+	if test -d vendor; then rm -rf vendor; fi
+	cp -r src vendor
+	find vendor -name '.git' -print -type d -exec rm -rf {} +
+	rm -rf src
+
+fmt:
+	# go fmt *.go
+	go fmt cmd/*.go
+
+bin: 	self
+	rm -rf bin/*
+	@GOPATH=$(GOPATH) go build -o bin/dump cmd/dump.go
---
+++ cmd/dump.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"encoding/csv"
+	"errors"
+	"flag"
+	"github.com/tidwall/gjson"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"sync"
+)
+
+func main() {
+
+	dump_hashtags := flag.Bool("hashtags", true, "...")
+	dump_mentions := flag.Bool("mentions", true, "...")
+
+	tweets := flag.String("tweets", "", "...")
+
+	flag.Parse()
+
+	fh, err := os.Open(*tweets)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer fh.Close()
+
+	body, err := ioutil.ReadAll(fh)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rsp := gjson.ParseBytes(body)
+
+	if !rsp.Exists() {
+		log.Fatal("Nothing to export")
+	}
+
+	count := len(rsp.Array())
+	remaining := count
+
+	mentions := new(sync.Map)
+	hashtags := new(sync.Map)
+
+	done_ch := make(chan bool)
+	err_ch := make(chan error)
+
+	rsp.ForEach(func(_, tw gjson.Result) bool {
+
+		go func(tw gjson.Result) {
+
+			defer func() {
+				done_ch <- true
+			}()
+
+			if *dump_mentions {
+				mentions_rsp := tw.Get("entities.user_mentions")
+
+				if !mentions_rsp.Exists() {
+					err_ch <- errors.New("Missing mentions")
+					return
+				}
+
+				for _, m := range mentions_rsp.Array() {
+					user_rsp := m.Get("screen_name")
+					user := user_rsp.String()
+					mentions.Store(user, true)
+				}
+			}
+
+			if *dump_hashtags {
+				hashtags_rsp := tw.Get("entities.hashtags")
+
+				if !hashtags_rsp.Exists() {
+					err_ch <- errors.New("Missing hashtags")
+					return
+				}
+
+				for _, h := range hashtags_rsp.Array() {
+					tag_rsp := h.Get("text")
+					tag := tag_rsp.String()
+					hashtags.Store(tag, true)
+				}
+			}
+		}(tw)
+
+		return true
+	})
+
+	for remaining > 0 {
+		select {
+		case <-done_ch:
+			remaining -= 1
+			// log.Printf("%d of %d remaining\n", remaining, count)
+		case err := <-err_ch:
+			log.Println(err)
+		default:
+			// pass
+		}
+	}
+
+	writers := []io.Writer{
+		os.Stdout,
+	}
+
+	multi := io.MultiWriter(writers...)
+	wr := csv.NewWriter(multi)
+
+	write_row := func(row []string) bool {
+
+		err := wr.Write(row)
+
+		if err != nil {
+			log.Println(err)
+			return false
+		}
+
+		return true
+	}
+
+	write_row([]string{ "property", "value" })
+	
+	mentions.Range(func(key interface{}, value interface{}) bool {
+
+		user := key.(string)
+
+		out := []string{
+			"user",
+			user,
+		}
+
+		return write_row(out)
+	})
+
+	hashtags.Range(func(key interface{}, value interface{}) bool {
+
+		tag := key.(string)
+
+		out := []string{
+			"tag",
+			tag,
+		}
+
+		return write_row(out)
+	})
+
+}
